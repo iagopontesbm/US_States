e90b12d8b88871505c4ba45852b0db2183f3b89b
Iago Pontes <iago.pontes@outlook.com>
2022-11-29 07:29:48 -0300

    1.1 - Reposta Pronta.

--- main.py
+++ main.py
@@ -4,22 +4,31 @@ import pandas as pd
 screen = tt.Screen()
 screen.title("U.S. States Game")
 image = "blank_states_img.gif"
-screen.addshape(image)
-tt.shape(image)
-map_text = tt.Turtle()
+screen.addshape(image)  # Adciona imagem as opções de shape
+tt.shape(image)  # Vincula o novo shape
+map_text = tt.Turtle()  # Texto no mapa
 
-answer_state = screen.textinput(title="Guess the State", prompt="What's another state's name.").title()
-print(answer_state)
+data = pd.read_csv("50_states.csv")  # Leitura e armazenado dados csv
+states_list = []  # Lista de estados a serem preenchidos
 
-data = pd.read_csv("50_states.csv")
+while len(states_list) != 50:  # Enquanto lista não estiver preenchida
+    answer_state = screen.textinput(title=f"{len(states_list)}/50 States Correct",  # Informa numero de acertos
+                                    prompt="What's another state's name.").title()  # Conveerte texto para title case
+    us_state = data[data.state == answer_state]  # Armazena a series do estado informado
+    state_name = False if us_state.empty else us_state.state.item()  # Nome do estado se for encontrado no DataFrame
+    if answer_state == state_name:  # Compara resposta informada e Dataframe
+        states_list.append(state_name)  # adiciona lista
+        x_pos = us_state.x.item()  # Posição X no mapa
+        y_pos = us_state.y.item()  # Posição Y no mapa
+        # Gerando texto no mapa
+        map_text.color("black")
+        map_text.penup()
+        map_text.goto(x_pos, y_pos)
+        map_text.hideturtle()
+        map_text.write(answer_state, align="center")
+    else:
+        continue
 
-us_state = data[data.state == answer_state]
-print(us_state.x.at)
-print(us_state.y.at)
+print("Congratulations!")
 
-
-# map_text.color("black")
-# map_text.penup()
-# map_text.goto(us_state.x, us_state.y)
-# map_text.hideturtle()
-#
+tt.mainloop()
